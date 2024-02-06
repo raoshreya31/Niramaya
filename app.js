9beffb7b28b52a1f2b1bf999057b5fb3b08a57ba
raoshreya31 <96344609+raoshreya31@users.noreply.github.com>
2024-02-06 23:43:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ const express=require('express');
 const cors = require('cors');
 const port=7000;
 const app=express();
-// const fs=require('fs');
-// const fileContent=fs.readFileSync('index.html');
-
 
 app.use(express.json());
 
@@ -19,7 +16,6 @@ app.post('/student-details',(req,res)=>{
 //     res.send('GET request to /student-details endpoint');
 //   });
   
-
 app.listen(port, ()=>{
     console.log(`Express server running on port ${port}`);
 });
